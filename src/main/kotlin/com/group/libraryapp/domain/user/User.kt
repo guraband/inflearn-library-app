ee4be62908c9d43d0b098e88e62a382fc31f9c7c
NG <guraband@naver.com>
2024-02-13 23:22:21 +0900

    Repository, Service 계층 리팩토링

--- src/main/kotlin/com/group/libraryapp/domain/user/User.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/User.kt
@@ -1,7 +1,7 @@
 package com.group.libraryapp.domain.user
 
 import com.group.libraryapp.domain.book.Book
-import com.group.libraryapp.domain.user.loanhistor.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import javax.persistence.*
 
 @Entity
--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -1,4 +1,4 @@
-package com.group.libraryapp.domain.user.loanhistor
+package com.group.libraryapp.domain.user.loanhistory
 
 import com.group.libraryapp.domain.user.User
 import javax.persistence.*
